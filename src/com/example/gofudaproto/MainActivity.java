26e6ddda5483743fe9e9a919f9502c8247bce918
IluvEDM <kangingugu@naver.com>
2015-06-08 02:32:36 +0900

    First Commit

---
+++ src/com/example/gofudaproto/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.gofudaproto;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+	private Fragment mPrevFragment;
+	private Fragment mStartFragment;
+	private Fragment mIntroFragment;
+	Boolean mIsHaveToBackFragment = false;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mIntroFragment = new IntroFragment();
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.container, mIntroFragment).commit();
+		}
+	}
+	public void setHaveToBack(Boolean isBack){
+		mIsHaveToBackFragment = isBack;
+	}
+	public void setStartFragment(Fragment fragment){
+		this.mStartFragment = fragment;
+	}
+	
+	public void setPrevFragment(Fragment fragment){
+		this.mPrevFragment = fragment;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public void onBackPressed() {
+		if(mIsHaveToBackFragment){
+			getSupportFragmentManager().beginTransaction()
+			.replace(R.id.container, mPrevFragment).commit();
+			mIsHaveToBackFragment = false;
+		}else{
+			// TODO Auto-generated method stub
+			super.onBackPressed();
+		}
+	}
+
+
+}
---
+++ src/com/example/gofudaproto/TruckFragment.java
@@ -0,0 +1,148 @@
+package com.example.gofudaproto;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass. Activities that contain this fragment
+ * must implement the {@link TruckFragment.OnFragmentInteractionListener}
+ * interface to handle interaction events. Use the
+ * {@link TruckFragment#newInstance} factory method to create an instance of
+ * this fragment.
+ *
+ */
+public class TruckFragment extends Fragment implements OnClickListener{
+	// TODO: Rename parameter arguments, choose names that match
+	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+	private static final String ARG_PARAM1 = "param1";
+	private static final String ARG_PARAM2 = "param2";
+
+	// TODO: Rename and change types of parameters
+	private String mParam1;
+	private String mParam2;
+	private MainActivity mParentActivity;
+	private OnFragmentInteractionListener mListener;
+	private Button mMainButton;
+	private Button mCallButton;
+	private Button mReviewButton;
+	private Button mRecipeButton;
+
+	/**
+	 * Use this factory method to create a new instance of this fragment using
+	 * the provided parameters.
+	 *
+	 * @param param1
+	 *            Parameter 1.
+	 * @param param2
+	 *            Parameter 2.
+	 * @return A new instance of fragment TruckFragment.
+	 */
+	// TODO: Rename and change types and number of parameters
+	public static TruckFragment newInstance(String param1, String param2) {
+		TruckFragment fragment = new TruckFragment();
+		Bundle args = new Bundle();
+		args.putString(ARG_PARAM1, param1);
+		args.putString(ARG_PARAM2, param2);
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	public TruckFragment() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (getArguments() != null) {
+			mParam1 = getArguments().getString(ARG_PARAM1);
+			mParam2 = getArguments().getString(ARG_PARAM2);
+		}
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_truck, container, false);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		mParentActivity = (MainActivity)getActivity();
+		mParentActivity.setHaveToBack(true);
+		mMainButton = (Button)mParentActivity.findViewById(R.id.bt_menu);
+		mMainButton.setOnClickListener(this);
+		mCallButton = (Button)mParentActivity.findViewById(R.id.bt_requirement);
+		mCallButton.setOnClickListener(this);
+		mReviewButton = (Button)mParentActivity.findViewById(R.id.bt_review);
+		mReviewButton.setOnClickListener(this);
+		mRecipeButton = (Button)mParentActivity.findViewById(R.id.bt_recipe);
+		mRecipeButton.setOnClickListener(this);
+	}
+
+	// TODO: Rename method, update argument and hook method into UI event
+	public void onButtonPressed(Uri uri) {
+//		if (mListener != null) {
+//			mListener.onFragmentInteraction(uri);
+//		}
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+//		try {
+//			mListener = (OnFragmentInteractionListener) activity;
+//		} catch (ClassCastException e) {
+//			throw new ClassCastException(activity.toString()
+//					+ " must implement OnFragmentInteractionListener");
+//		}
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		mListener = null;
+	}
+
+	/**
+	 * This interface must be implemented by activities that contain this
+	 * fragment to allow an interaction in this fragment to be communicated to
+	 * the activity and potentially other fragments contained in that activity.
+	 * <p>
+	 * See the Android Training lesson <a href=
+	 * "http://developer.android.com/training/basics/fragments/communicating.html"
+	 * >Communicating with Other Fragments</a> for more information.
+	 */
+	public interface OnFragmentInteractionListener {
+		// TODO: Update argument type and name
+		public void onFragmentInteraction(Uri uri);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+		case R.id.bt_menu:
+			break;
+		case R.id.bt_requirement:
+			break;
+		case R.id.bt_review:
+			break;
+		case R.id.bt_recipe:
+			break;
+		
+		}
+		
+	}
+
+}
