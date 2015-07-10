60fc44314e491e78cd6a396659a209739dcdfd2a
iluvEDM <kangingu@kanginguui-MacBook-Pro.local>
2015-07-10 17:19:59 +0900

    푸시 초기화 준비

--- src/com/example/gofudaproto/MainActivity.java
+++ src/com/example/gofudaproto/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.gofudaproto;
 
+import java.io.IOException;
+
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapReverseGeoCoder;
 import net.daum.mf.map.api.MapView;
 
 import com.example.gofudaproto.server.ServerManager;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.nhn.android.maps.maplib.NGeoPoint;
 
 import android.support.v7.app.ActionBarActivity;
@@ -14,10 +19,13 @@ import android.support.v4.app.Fragment;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -27,6 +35,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.os.Build;
 import android.provider.Settings;
@@ -40,6 +49,21 @@ public class MainActivity extends ActionBarActivity{
 	public boolean isHaveToUseCurrentLocation =false;
 	public static final String CLIENT_REQUEST_ID_INDEX = "gopuda_client_current_request_id";
 	public static final String API_KEY = "867b82fb3cbe6e4b39c4e90405b6bc5d";
+	private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+	// SharedPreferences에 저장할 때 key 값으로 사용됨.
+    public static final String PROPERTY_REG_ID = "registration_id";
+
+    // SharedPreferences에 저장할 때 key 값으로 사용됨.
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private static final String TAG = "ICELANCER";
+    String SENDER_ID = "Your-Sender-ID";
+
+    GoogleCloudMessaging gcm;
+    Context context;
+
+    String regid;
+    private TextView mDisplay;
+	
 	private net.daum.mf.map.api.MapView mMapView;
 	private int beforeContainerID;
 	private ServerManager mServerManager;
@@ -71,7 +95,107 @@ public class MainActivity extends ActionBarActivity{
 		mServerManager = new ServerManager();
 		
 		gps = new GpsInfo(MainActivity.this);
+		
+		if (checkPlayServices()) {
+			gcm = GoogleCloudMessaging.getInstance(this);
+            regid = getRegistrationId(context);
+
+            if (regid.isEmpty()) {
+                registerInBackground();
+            }else {
+                Log.i(TAG, "No valid Google Play Services APK found.");
+            }
+        }
+
         
+	}
+	private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        
+        // 앱이 업데이트 되었는지 확인하고, 업데이트 되었다면 기존 등록 아이디를 제거한다.
+        // 새로운 버전에서도 기존 등록 아이디가 정상적으로 동작하는지를 보장할 수 없기 때문이다.
+        int registeredVersion = mPreference.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+        return registrationId;
+    }
+
+    private SharedPreferences getGCMPreferences(Context context) {
+        return getSharedPreferences(MainActivity.class.getSimpleName(),
+                Context.MODE_PRIVATE);
+    }
+
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+    private void registerInBackground() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+                    regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+
+                    // 서버에 발급받은 등록 아이디를 전송한다.
+                    // 등록 아이디는 서버에서 앱에 푸쉬 메시지를 전송할 때 사용된다.
+                    sendRegistrationIdToBackend();
+
+                    // 등록 아이디를 저장해 등록 아이디를 매번 받지 않도록 한다.
+                    storeRegistrationId(context, regid);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // If there is an error, don't just keep trying to register.
+                    // Require the user to click a button again, or perform
+                    // exponential back-off.
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                mDisplay.append(msg + "\n");
+            }
+
+        }.execute(null, null, null);
+    }
+
+    private void storeRegistrationId(Context context, String regid) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regid);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
+    private void sendRegistrationIdToBackend() {
+
+    }
+	@Override
+	protected void onPostResume() {
+		// TODO Auto-generated method stub
+		super.onPostResume();
+		checkPlayServices();
 	}
 	public int getCurrentRequirementId(){
 		return mPreference.getInt(CLIENT_REQUEST_ID_INDEX, 0);
@@ -104,6 +228,20 @@ public class MainActivity extends ActionBarActivity{
         }
         return null;
 	}
+	private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i("ICELANCER", "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
 	public GpsInfo getGPS(){
 		return gps;
 	}
